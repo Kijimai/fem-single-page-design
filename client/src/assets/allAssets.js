0b89118419fdb9799ff9bbf4ad58da873ad8279f
John Deen Bucog <JDBucog@yahoo.com>
2022-08-10 01:02:29 -0700

    specialties

---
+++ client/src/assets/allAssets.js
@@ -0,0 +1,8 @@
+import graphicDesign from "./pattern-graphic-design.svg"
+import apps from "./pattern-apps.svg"
+import illustrations from "./pattern-illustrations.svg"
+import motionGraphics from "./pattern-motion-graphics.svg"
+import photography from "./pattern-photography.svg"
+import uiUx from "./pattern-ui-ux.svg"
+
+export { graphicDesign, apps, illustrations, motionGraphics, photography, uiUx }
--- client/src/components/Feature/Specialties.jsx
+++ client/src/components/Feature/Specialties.jsx
@@ -1,26 +1,68 @@
 import React from "react"
+import {
+  graphicDesign,
+  apps,
+  illustrations,
+  motionGraphics,
+  photography,
+  uiUx,
+} from "../../assets/allAssets"
 
 const Specialties = () => {
   return (
     <section>
       <div className="grid specialties">
         <div className="specialties__gdesign">
-          <span className="specialties__text">Graphic Design</span>
+          <div className="specialties__content">
+            <img
+              className="specialties__image"
+              src={graphicDesign}
+              alt="Graphic design"
+            />
+            <span className="specialties__text"> Graphic Design</span>
+          </div>
         </div>
         <div className="specialties__ui-ux">
-          <span className="specialties__text">UI/UX</span>
+          <div className="specialties__content">
+            <img className="specialties__image" src={uiUx} alt="UI/UX" />
+            <span className="specialties__text">UI/UX</span>
+          </div>
         </div>
         <div className="specialties__apps">
-          <span className="specialties__text">Apps</span>
+          <div className="specialties__content">
+            <img className="specialties__image" src={apps} alt="Apps" />
+            <span className="specialties__text">Apps</span>
+          </div>
         </div>
         <div className="specialties__illustrations">
-          <span className="specialties__text">Illustrations</span>
+          <div className="specialties__content">
+            <img
+              className="specialties__image"
+              src={illustrations}
+              alt="Illustrations"
+            />
+            <span className="specialties__text">Illustrations</span>
+          </div>
         </div>
         <div className="specialties__photography">
-          <span className="specialties__text">Photography</span>
+          <div className="specialties__content">
+            <img
+              className="specialties__image"
+              src={photography}
+              alt="Photography"
+            />
+            <span className="specialties__text">Photography</span>
+          </div>
         </div>
         <div className="specialties__motion">
-          <span className="specialties__text">Motion Graphics</span>
+          <div className="specialties__content">
+            <img
+              className="specialties__image"
+              src={motionGraphics}
+              alt="Motion graphics"
+            />
+            <span className="specialties__text">Motion Graphics</span>
+          </div>
         </div>
       </div>
     </section>
