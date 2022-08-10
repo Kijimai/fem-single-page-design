da2b564a752ef663d339681ba3ed8850235ad357
John Deen Bucog <JDBucog@yahoo.com>
2022-08-09 19:44:11 -0700

    media queries for 768px and above need fixing

--- client/src/components/Feature/Specialties.jsx
+++ client/src/components/Feature/Specialties.jsx
@@ -4,7 +4,24 @@ const Specialties = () => {
   return (
     <section>
       <div className="grid specialties">
-
+        <div className="specialties__gdesign">
+          <span className="specialties__text">Graphic Design</span>
+        </div>
+        <div className="specialties__ui-ux">
+          <span className="specialties__text">UI/UX</span>
+        </div>
+        <div className="specialties__apps">
+          <span className="specialties__text">Apps</span>
+        </div>
+        <div className="specialties__illustrations">
+          <span className="specialties__text">Illustrations</span>
+        </div>
+        <div className="specialties__photography">
+          <span className="specialties__text">Photography</span>
+        </div>
+        <div className="specialties__motion">
+          <span className="specialties__text">Motion Graphics</span>
+        </div>
       </div>
     </section>
   )
